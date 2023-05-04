6bc924d10e7138c64d6096bf8e9bc89a1464e390
Yuki Watanabe <>
2023-05-04 23:36:30 +0900

    chore: OpenAPI, Swagger の導入

--- src/main/kotlin/com/fluffy/plugins/Routing.kt
+++ src/main/kotlin/com/fluffy/plugins/Routing.kt
@@ -3,11 +3,15 @@ package com.fluffy.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.swagger.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+        openAPI(path="openapi", swaggerFile = "_build/openapi.yaml")
+        swaggerUI(path = "swagger", swaggerFile = "_build/openapi.yaml")
     }
 }
